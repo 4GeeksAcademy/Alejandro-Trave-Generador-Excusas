b5d70c9ea35cf3d941fdab0ff70b6b2ff3a42996
Alejandro Travé <121689156+Atravep@users.noreply.github.com>
2024-03-26 18:00:41 +0000

    fix-> propietario:4Geeks

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["Mi perro", "Un bombero", "Un kaiju", "El loco del barrio"];
+let action = ["se comió", "babeó", "robó", "quemó"];
+let what = [
+  "mis tareas",
+  "las llaves de mi casa",
+  "el coche",
+  "la tarta de cumpleaños"
+];
+let when = [
+  "antes de clase",
+  "tras la caída del Imperio Romano",
+  "mientras me duchaba",
+  "justo antes de que llegase el meteorito"
+];
+
+function creaExcusa() {
+  let who_random = who[Math.floor(Math.random() * who.length)];
+  let action_random = action[Math.floor(Math.random() * action.length)];
+  let what_random = what[Math.floor(Math.random() * what.length)];
+  let when_random = when[Math.floor(Math.random() * when.length)];
+
+  let nuevoWho = who_random + " ";
+  let nuevoComo = action_random + " ";
+  let nuevaCosa = what_random + " ";
+  let nuevoCuando = when_random;
+
+  document.getElementById("quien").innerHTML = nuevoWho;
+  document.getElementById("como").innerHTML = nuevoComo;
+  document.getElementById("cosa").innerHTML = nuevaCosa;
+  document.getElementById("cuando").innerHTML = nuevoCuando;
+}
+
+window.onload = creaExcusa;
